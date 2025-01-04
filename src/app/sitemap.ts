b638d9d870110bb01d36a560170beacf244a06c8
wiibleyde <nathan@bonnell.fr>
2025-01-04 02:19:26 +0100

    Add robots and sitemap generation functionality with dynamic URL handling
    Signed-off-by: wiibleyde <nathan@bonnell.fr>

---
+++ src/app/sitemap.ts
@@ -0,0 +1,31 @@
+import { routing } from "@/i18n/routing";
+
+export const BASE_URL = 'https://portfolio.bonnell.fr';
+
+export default async function sitemap() {
+    const locales = routing.locales;
+    const paths = [
+        '/',
+        '/projects',
+    ];
+    const urls = locales.flatMap(locale => paths.map(path => `${BASE_URL}/${locale}${path}`));
+    return urls.map(url => {
+        const path = paths.find(path => url.endsWith(path)) as string;
+        return {
+            url,
+            lastmod: new Date().toISOString(),
+            priority: getPriority(path)
+        };
+    });
+}
+
+function getPriority(path: string): number {
+    switch (path) {
+        case '/':
+            return 1;
+        case '/projects':
+            return 0.9;
+        default:
+            return 0.5;
+    }
+}
