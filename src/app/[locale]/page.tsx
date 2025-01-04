940e1981161d17b11020162e30bd44c52be94e7a
wiibleyde <nathan@bonnell.fr>
2025-01-04 01:59:58 +0100

    Update background gradient opacity and reorder soft skills section in Home component
    Signed-off-by: wiibleyde <nathan@bonnell.fr>

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     const t = useTranslations('HomePage');
 
     return (
-        <div className='flex flex-col min-h-screen bg-no-repeat bg-fixed overflow-x-hidden' style={{ background: 'radial-gradient(circle, rgba(34, 139, 34, 0.3) 20%, rgba(0, 0, 0, 1) 80%)', backgroundSize: 'cover', backgroundPosition: 'top' }}>
+        <div className='flex flex-col min-h-screen bg-no-repeat bg-fixed overflow-x-hidden' style={{ background: 'radial-gradient(circle, rgba(34, 139, 34, 0.8) 20%, rgba(0, 0, 0, 1) 80%)', backgroundSize: 'cover', backgroundPosition: 'top' }}>
             <div className='flex-grow mb-auto flex flex-col bg-fixed'>
                 <div className='h-screen w-full bg-opacity-60 bg-black flex flex-col justify-center items-center'>
                     <h1 className='text-white text-8xl font-bold flex-grow flex items-center justify-center'>{t('title')}</h1>
@@ -24,12 +24,12 @@ export default function Home() {
                 </div>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-8 w-full'>
                     <div className=''>
-                        <h1 className='text-4xl font-bold mb-4 text-end'>{t('softskills')}</h1>
-                        <SoftSkills />
-                    </div>
-                    <div className=''>
                         <h1 className='text-4xl font-bold mb-4'>{t('hardskills')}</h1>
                         <HardSkills />
+                    </div>
+                    <div className=''>
+                        <h1 className='text-4xl font-bold mb-4 text-end'>{t('softskills')}</h1>
+                        <SoftSkills />
                     </div>
                 </div>
             </div>
