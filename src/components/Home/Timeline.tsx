6407d30eb34389ec1bc703d0203c453721a085f8
wiibleyde <nathan@bonnell.fr>
2025-01-05 12:26:30 +0100

    Remove old data from Timeline component and update message import path in i18n configuration
    Signed-off-by: wiibleyde <nathan@bonnell.fr>

--- src/components/Home/Timeline.tsx
+++ src/components/Home/Timeline.tsx
@@ -13,43 +13,6 @@ interface Timeline {
     class: string;
 }
 
-const oldData: Timeline[] = [
-    {
-        title: "Développeur Fullstack",
-        duration: "Septembre 2024 - Aujourd'hui",
-        org: "Lex-Port",
-        icon: Briefcase,
-        description: "Développement de sites web pour les clients de l'entreprise.",
-        skills: ["Next.js", "React", "TailwindCSS", "TypeScript", "Node.js"],
-        class: "bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300",
-    },
-    {
-        title: "Mastère Informatique",
-        duration: "Septembre 2022 - Septembre 2027",
-        org: "Ynov Campus",
-        description: "Développement d'applications web, mobile et logiciel (apprentissage néanmoins de technologies d'infrastructures et de réseaux).",
-        skills: ["Développement Web", "Développement Mobile", "Développement Logiciel"],
-        class: "bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300",
-    },
-    {
-        title: "Maraude de l'Ordre de Malte",
-        duration: "Septembre 2019 - Aujourd'hui",
-        org: "Ordre de Malte",
-        icon: HeartFill,
-        description: "Aide aux sans-abris de Bordeaux.",
-        skills: ["Écoute", "Aide", "Partage"],
-        class: "bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300",
-    },
-    {
-        title: "Baccalauréat Général",
-        duration: "Septembre 2007 - Juin 2022",
-        org: "Sainte Marie Grand Lebrun",
-        icon: Book,
-        skills: ["Numérique et Sciences Informatiques", "Historie-Géographie Géopolitique et Sciences Politiques", "Sciences économiques et sociales"],
-        class: "bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300",
-    },
-];
-
 export function Timeline() {
     const t = useTranslations("Timeline");
 
