3999279805e59449e1490cc4489a5e2e95b5b10c
wiibleyde <nathan@bonnell.fr>
2025-01-06 16:26:12 +0100

    incredible thing
    Signed-off-by: wiibleyde <nathan@bonnell.fr>

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -10,7 +10,10 @@ export default function Home() {
     const t = useTranslations('HomePage');
 
     return (
-        <div className='flex flex-col min-h-screen bg-no-repeat bg-fixed overflow-x-hidden' style={{ background: 'radial-gradient(circle, rgba(34, 139, 34, 0.8) 20%, rgba(0, 0, 0, 1) 80%)', backgroundSize: 'cover', backgroundPosition: 'top' }}>
+        <div className='flex flex-col min-h-screen bg-no-repeat bg-fixed overflow-x-hidden'>
+            <video autoPlay muted loop className='absolute -z-10 w-full h-full object-cover'>
+                <source src={"/video/background.mp4"} type='video/mp4' />
+            </video>
             <div className='flex-grow mb-auto flex flex-col bg-fixed'>
                 <div className='h-screen w-full bg-opacity-60 bg-black flex flex-col justify-center items-center'>
                     <h1 className='text-white text-8xl font-bold flex-grow flex items-center justify-center'>{t('title')}</h1>
