a6d86cb851cae4fd1d897311603452d45362bb53
wiibleyde <nathan@bonnell.fr>
2025-01-20 15:18:13 +0100

    Update robots.txt configuration to specify allowed and disallowed paths for web crawlers
    Signed-off-by: wiibleyde <nathan@bonnell.fr>

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,16 @@
+import { MetadataRoute } from "next";
 import { BASE_URL } from "./sitemap";
 
-export default function robots() {
+export default function robots():MetadataRoute.Robots {
     return {
         rules: [
             {
                 userAgent: '*',
+                allow: '/',
+                disallow: [
+                    '/api',
+                    '/_next',
+                ],
             },
         ],
         sitemap: `${BASE_URL}/sitemap.xml`,
