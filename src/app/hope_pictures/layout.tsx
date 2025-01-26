19850937599805cf2c3c61013ff8dd289ce4ac71
Wiibleyde <nathan@bonnell.fr>
2025-01-26 15:28:19 +0100

    Add picture browsing feature with API for folder and file retrieval
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ src/app/hope_pictures/layout.tsx
@@ -0,0 +1,27 @@
+import { Montserrat } from 'next/font/google'
+import "@/app/globals.css"
+import { Metadata } from 'next';
+
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+export const metadata: Metadata = {
+    title: "Photos de Hope - GTARP",
+    description: "Photos de Hope, serveur GTARP",
+}
+
+export default async function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang='fr'>
+            <body className={montserrat.className}>
+                {children}
+            </body>
+        </html>
+    );
+}
