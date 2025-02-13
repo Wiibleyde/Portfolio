c087294a2eba2708db9bcaecce11db1ac8fb9c4c
Wiibleyde <nathan@bonnell.fr>
2025-02-13 16:58:44 +0100

    Update keyboard shortcuts for Mac and Linux in the Home page
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- src/app/lyp/page.tsx
+++ src/app/lyp/page.tsx
@@ -39,10 +39,10 @@ export default function Home() {
                         <p className="mb-2">Press <span className="font-bold rainbow-text">Win + L</span></p>
                     )}
                     {os === OsType.Mac && (
-                        <p>Press <span className="font-bold rainbow-text">Ctrl + Cmd + Q</span></p>
+                        <p>Press <span className="font-bold rainbow-text">Cmd + Ctrl + Q</span></p>
                     )}
                     {os === OsType.Linux && (
-                        <p>Press <span className="font-bold rainbow-text">Ctrl + Alt + L</span></p>
+                        <p>Press <span className="font-bold rainbow-text">Meta + L / Ctrl + Alt + L</span></p>
                     )}
                     {os === OsType.Unknown && (
                         <p className="text-2xl">Unknown OS <span className="font-bold rainbow-text">but you can still lock your computer</span></p>
