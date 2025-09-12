1f0fe949a2dcb3f201f4cd6585b4296ac3903db2
Wiibleyde <nathan@bonnell.fr>
2025-09-12 18:28:45 +0200

    feat: add image domain configuration to next.js config
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const isDocker = process.env.IS_DOCKER === 'true';
 
 const nextConfig: NextConfig = {
     output: isDocker ? 'standalone' : undefined,
+    images: {
+        domains: ['static-cdn.jtvnw.net', 'cdn.frankerfacez.com', 'cdn.betterttv.net', 'nathan.bonnell.fr'],
+    },
 };
 
 export default nextConfig;
