6924177a6914f80d946f5c03e37226d2021e229a
Wiibleyde <nathan@bonnell.fr>
2025-09-12 18:29:23 +0200

    feat: update image configuration to use remotePatterns in next.js config
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,28 @@ const isDocker = process.env.IS_DOCKER === 'true';
 const nextConfig: NextConfig = {
     output: isDocker ? 'standalone' : undefined,
     images: {
-        domains: ['static-cdn.jtvnw.net', 'cdn.frankerfacez.com', 'cdn.betterttv.net', 'nathan.bonnell.fr'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'static-cdn.jtvnw.net',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.frankerfacez.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.betterttv.net',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'nathan.bonnell.fr',
+                pathname: '/**',
+            },
+        ],
     },
 };
 
